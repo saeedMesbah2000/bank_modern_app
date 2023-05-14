563449edbb62ccf8a9df03fa7d63a0d013104ccf
saeed <saeed@mary.local>
2023-05-14 22:13:12 +0330

    completing stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {stats} from "../constans";
+import styles from "../style";
 
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section
+      className={`${styles.flexCenter} sm:flex-row flex-col flex-wrap sm:mb-20 mb-6`}>
+      {stats.map((stat, index) => {
+        return (
+          <div
+            key={stat.id}
+            className={`flex-1 flex justify-start items-center flex-row`}>
+            <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading[53px] leading-[43px] text-white">
+              {stat.value}
+            </h4>
+            <p className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3">
+              {stat.title}
+            </p>
+          </div>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Stats;
