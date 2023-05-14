76ecf33cff34295e71e3104ef070f6dc65643162
saeed <saeed@mary.local>
2023-05-14 10:14:54 +0330

    completing hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section id="home" className={``}>
+      Hero
+    </section>
+  );
 };
 
 export default Hero;
