529de665d413bbd5d12a75faa6856d6834db8082
saeed <saeed@mary.local>
2023-05-14 23:12:18 +0330

    completing Business and Button component and creating FeatureCard component and completing it

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import {features} from "../constans";
+import styles, {layout} from "../style";
+import Button from "./Button";
+import FeatureCard from "./FeatureCard";
 
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="sm:block hidden" /> we’ll handle
+          the money.
+        </h2>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+
+        <Button styles="mt-10" text="Get Started" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => {
+          return <FeatureCard key={feature.id} {...feature} index={index} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Business;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({styles, text}) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-xl ${styles}`}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
