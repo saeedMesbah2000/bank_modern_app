707cf92746fb1a5340c5f6cfc01330464d15c8c7
saeed <saeed@mary.local>
2023-05-15 13:56:27 +0330

    completing Clients component

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {clients} from "../constans";
+import styles from "../style";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client, index) => {
+          return (
+            <div
+              key={client.id}
+              className={`flex-1 ${styles.flexCenter} sm:w-[192px] w-[120px]`}>
+              <img
+                src={client.logo}
+                alt="logo"
+                className="sm:w-[192px] w-[100px] object-contain px-2 py-2 rounded-[12px] transition ease-out duration-500 hover:bg-dimWhite cursor-pointer"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
